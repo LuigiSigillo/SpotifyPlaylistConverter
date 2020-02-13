b319e74c9e7afadba48d23bffefda9d8112d1721
LuigiSigillo <luigisigillo30@gmail.com>
2020-02-13 19:39:23 +0100

    refactor of gmusic

--- gmusic.py
+++ gmusic.py
@@ -3,24 +3,6 @@ from gmusicapi import Webclient
 import json
 import yaml
 
-with open("config.yml", 'r') as ymlfile:
-    cfg = yaml.load(ymlfile)
-
-mc = Mobileclient()
-#mc.perform_oauth()
-
-mc.oauth_login(device_id=cfg['provagmusic']['device_id'])
-
-#"7f30ce56-a743-3326-88ff-8a6d94d9c111"
-songs = mc.get_all_songs()
-#print(len(songs))
-
-'''with open('data.json', 'w', encoding="utf-8") as fp:
-    json.dump(songs, fp, sort_keys=True, indent=4,ensure_ascii=False)
-'''
-with open('./playlists/data.json', 'r', encoding="utf-8") as fp:
-    data = json.load(fp)
-
 def get_songs_info(songs, id_song):
     for e in songs:
         if e['id'] == id_song:
@@ -31,26 +13,58 @@ def get_songs_info(songs, id_song):
                 'id': e['id']
             }
 
-
 def write_playlists(songs):
     playlists = mc.get_all_user_playlist_contents()
+    names = []
     for p in playlists:
         p_dict_list = []
         name = p["name"]
         for track in p['tracks']:
-                p_dict_list.append(get_songs_info(songs,track['trackId']))
+            p_dict_list.append(get_songs_info(songs,track['trackId']))
         
         with open("./playlists/" + p["name"]+'.json', 'w', encoding="utf-8") as fp:
             json.dump(p_dict_list, fp, sort_keys=True, indent=4, ensure_ascii=False)
-        
+        names.append(name)
+    return names
 
-write_playlists(data)
+def init():
+    with open("config.yml") as ymlfile:
+        cfg = yaml.load(ymlfile)
+        logged = cfg['provagmusic']['logged']
+        print(type(logged),logged)
+    mc = Mobileclient()
+    if not logged:
+        cfg['provagmusic']['logged'] = True
+        with open("config.yml") as ymlfile:
+            yaml.dump(cfg,ymlfile)
+        mc.perform_oauth()
+    else:
+        mc.oauth_login(device_id=cfg['provagmusic']['device_id'])
 
-def get_songs_of_playlist(playlist_name):
+    #"7f30ce56-a743-3326-88ff-8a6d94d9c111"
+    with open('./playlists/data.json', 'r', encoding="utf-8") as fp:
+        data = json.load(fp)
+    if len(data) == 0:
+        data = mc.get_all_songs()
+        with open('./playlists/data.json', 'w', encoding="utf-8") as fp:
+            json.dump(data, fp, sort_keys=True, indent=4, ensure_ascii=False)
+    return data
+
+
+if __name__ == "__main__":
+    data = init()
+    return write_playlists(data)
+
+
+
+
+
+''' not used for now'''
+def get_songs_of_playlist(playlist_name,songs):
     playlists = mc.get_all_user_playlist_contents()
     for p in playlists:
         p_dict_list = []
         if playlist_name == p["name"]:
             for track in p['tracks']:
-                p_dict_list.append(get_songs_info(track['trackId']))
+                p_dict_list.append(get_songs_info(songs,track['trackId']))
             return p_dict_list
